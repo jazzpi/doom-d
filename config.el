1461d884e90c0eed3fa1f5af536178d7de8b3e63
jazzpi <jasper@mezzo.de>
2019-03-14 11:40:19 +0100

    Open .launch files in XML mode

--- config.el
+++ config.el
@@ -2,24 +2,29 @@
 
 (setq doom-font (font-spec :name "Meslo LG S DZ" :size 12 :weight 'light :width 'normal))
 
+;;; PACKAGE CONFIGURATION
 (after! which-key
   (setq which-key-idle-delay 0.5
         which-key-idle-secondary-delay 0.0))
+
+(after! magit
+  (setq magithub-clone-default-directory "~/dev"
+        magithub-preferred-remote-method 'ssh_url))
 
 (setq +org-dir (expand-file-name "~/Documents/org")
       ;+rust-src-dir (expand-file-name "~/dev/rust/")
       helm-move-to-line-cycle-in-source nil
       )
 
-(with-eval-after-load "config/default/+evil-bindings" (load! "+bindings"))
-
 ;; Evil doesn't really work in the terminal
 (add-to-list 'evil-emacs-state-modes 'term-mode)
 
-;; (setq +magit-hub-enable-by-default 't
-;;       +magit-hub-features 't)
-(after! magit
-  (setq magithub-clone-default-directory "~/dev"
-        magithub-preferred-remote-method 'ssh_url))
+;;; MODULE CONFIGURATION
+(with-eval-after-load "config/default/+evil-bindings" (load! "+bindings"))
+
+;;; ROS CONFIGURATION
+(add-to-list 'auto-mode-alist '("\\.launch$" . xml-mode))
+
+;;; LANGUAGE CONFIGURATION
 
 (add-hook! 'c-mode-common-hook (load! "lang/+c"))
