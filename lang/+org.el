f6c057dedf325fc0b05f2e4712d1753ec98354b1
jazzpi <jasper@mezzo.de>
2024-02-03 16:55:41 +0100

    Paste image from clipboard

--- lang/+org.el
+++ lang/+org.el
@@ -18,6 +18,15 @@
                 (replace-regexp-in-string "%latex " "%latex -shell-escape " s))
               org-latex-pdf-process))
 
+; see https://emacs.stackexchange.com/a/77575/15775
+(after! org-download
+      (setq org-download-method 'directory)
+      (setq org-download-image-dir (concat (file-name-sans-extension (buffer-file-name)) "-img"))
+      (setq org-download-image-org-width 600)
+      (setq org-download-link-format "[[file:%s]]\n"
+        org-download-abbreviate-filename-function #'file-relative-name)
+      (setq org-download-link-format-function #'org-download-link-format-function-default))
+
 ; see https://emacs.stackexchange.com/a/19941/15775
 ;; (defun jazzpi/minted-options (text backend info)
 ;;   "Add options to minted environment"
