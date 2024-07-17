139a247cdc2e72981887f4e3f76400ace943d60e
jazzpi <jasper@mezzo.de>
2024-07-17 12:55:42 +0200

    Enable LSP for LaTeX

--- init.el
+++ init.el
@@ -112,7 +112,7 @@
        (lookup           ; navigate your code and its documentation
         +dictionary
         +docsets)
-       ;;lsp
+       (lsp +eglot)
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -161,7 +161,7 @@
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex            ; writing papers in Emacs has never been so fun
-        +latexmk)
+        +lsp)
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
