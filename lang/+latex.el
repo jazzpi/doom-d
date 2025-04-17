cb579d5f370dade2fd5a1413a28a04bb7b82dca4
jazzpi <jasper@mezzo.de>
2025-04-17 22:04:00 +0200

    Add TeX command for latexmk with RC file

--- lang/+latex.el
+++ lang/+latex.el
@@ -1,1 +1,14 @@
 ;;; ~/.doom.d/lang/+latex.el -*- lexical-binding: t; -*-
+
+(add-to-list
+ 'TeX-command-list
+ '("LaTeXMk (with rc file)"
+   "latexmk -f"
+   TeX-run-command
+   nil ; ask for confirmation
+   t ; active in all modes
+   :help "Run latexmk without any arguments (probably requires a latexmkrc file)"))
+(after! hl-todo
+  (add-to-list 'hl-todo-keyword-faces
+               '("\\todo" warning bold)))
+(hl-todo-mode)
